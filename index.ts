795624fe6d99fa10f0783fe43ba6a3ae3d95824e
bersling <bersling@gmail.com>
2017-06-17 10:57:51 +0200

    move index

---
+++ index.ts
@@ -0,0 +1,3 @@
+export {DatabaseResponse, DatabaseError, ReadResponse, CreateResponse, UpdateResponse} from './src/database-response.model'
+export {DatabaseConfig} from './src/database-config.model'
+export {dao} from './src/dao'
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export {DatabaseResponse, DatabaseError, ReadResponse, CreateResponse, UpdateResponse} from './database-response.model'
-export {DatabaseConfig} from './database-config.model'
-export {dao} from './dao'
