2eb4c0a85fd0d2993025815d35b76bee8db199f4
bersling <bersling@gmail.com>
2017-06-17 10:52:50 +0200

    first version

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export {DatabaseResponse, DatabaseError, ReadResponse, CreateResponse, UpdateResponse} from './database-response.model'
+export {DatabaseConfig} from './database-config.model'
+export {dao} from './dao'
